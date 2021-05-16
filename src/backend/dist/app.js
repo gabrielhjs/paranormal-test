72c8fafc2293e8fbe4eb88552606e013992dc2d4
Gabriel Sá <gabriel.sa@laura-br.com>
2021-05-16 19:32:50 -0300

    testing

--- src/backend/dist/app.js
+++ src/backend/dist/app.js
@@ -21,7 +21,7 @@ app.use(compression_1.default({
     }
 }));
 app.use((request, response, next) => {
-    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Origin", `${process.env.HOST}`);
     response.header("Access-Control-Allow-Headers", "X-Requested-With");
     next();
 });
--- src/backend/dist/server.js
+++ src/backend/dist/server.js
@@ -10,5 +10,9 @@ const connect_1 = require("./typeorm/connect");
 const app_1 = require("./app");
 dotenv_1.default.config();
 connect_1.typeormConnection.create(process.env.NODE_ENV || "default");
-app_1.app.use(cors_1.default());
+app_1.app.use(cors_1.default({
+    origin: [
+        `${process.env.HOST}`,
+    ]
+}));
 app_1.app.listen(3333);
